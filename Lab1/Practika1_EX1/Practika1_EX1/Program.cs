c4bd996118ffb998b866aead252107f0b3d1c784
DR-KOKS <107673754+DR-KOKS@users.noreply.github.com>
2022-06-17 10:45:09 +0300

    Add files via upload

---
+++ Lab1/Practika1_EX1/Practika1_EX1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practika1_EX1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string otv;
+        
+            while((otv=Console.ReadLine())!=null) 
+            {              
+                otv = otv.Replace(",", " y:");
+                otv = "x:" + otv;
+                Console.WriteLine(otv);
+                
+            }
+            
+        }
+    }
+}
---
+++ Lab1/Practika1_EX2/Practika1_EX2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practika1_EX2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string otv = textBox1.Text;
+
+            otv = otv.Replace(",", " y:");
+            otv = "x:" + otv;
+
+            textBlock1.Text = otv;
+        }
+
+        private void textBlock1_Loaded(object sender, RoutedEventArgs e)
+        {
+            string otv;
+            while ((otv = Console.ReadLine()) != null)
+            {
+                otv = otv.Replace(",", " y:");
+                otv = "x:" + otv;
+                textBlock1.Text += otv;
+            }
+        }
+    }
+}
